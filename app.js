29752c7b1539ae9b6366f67ac5d52fe7737ec0d2
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-03-31 12:16:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.use(cookieParser());
+
+
+// app.get("*", (req, res) =>
+//     res.sendFile(path.resolve("dist", "index.html"))
+//   );
+  // Passport Local Strategy
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
+
+module.exports = app;
+

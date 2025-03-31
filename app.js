e0e4e34c4b694f14d44ce544669c7c201f0f181a
Shivansh9848 <104058319+shivansh9848@users.noreply.github.com>
2025-03-31 13:16:31 +0530

    fix: update script source in index.html and format code in app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const cookieParser = require('cookie-parser');
-const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes');
-const captainRoutes = require('./routes/captain.routes');
-const mapsRoutes = require('./routes/maps.routes');
-const rideRoutes = require('./routes/ride.routes');
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
+const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
 
 connectToDb();
 
@@ -21,17 +21,12 @@ app.use(express.static(path.resolve(__dirname, "dist")));
 
 app.use(cookieParser());
 
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
-app.get("*", (req, res) =>
-    res.sendFile(path.resolve("dist", "index.html"))
-  );
-app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
-app.use('/maps', mapsRoutes);
-app.use('/rides', rideRoutes);
-
-
-
+app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
+//
 
 module.exports = app;
-
